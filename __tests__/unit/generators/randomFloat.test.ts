ae18cf01c195fbfc847193c819cdb8eab3bda84e
Patrick Caldwell <tncbbthositg@gmail.com>
2020-07-13 12:31:52 -0400

    patrick | no card | testing random number distribution on randomFloat

--- __tests__/unit/generators/randomFloat.test.ts
+++ __tests__/unit/generators/randomFloat.test.ts
@@ -4,13 +4,25 @@ const SAMPLE_SIZE = 100000;
 
 describe(randomFloat, () => {
   it('should pseudorandomly select an float between two bounds with an exclusive max', () => {
+    const results: { [key: number]: number } = {};
+
     for (let i = 0; i < SAMPLE_SIZE; ++i) {
       const value = randomFloat("11", "18");
-      const isValid = value >= 11 && value < 18;
-      if (!isValid) {
-        expect(value).toBeGreaterThanOrEqual(11);
-        expect(value).toBeLessThan(18);
+      const group = Math.floor(value);
+
+      if (typeof results[group] === 'undefined') {
+        results[group] = 0;
       }
+
+      results[group]++;
+    }
+
+    const expectedKeys = ['11', '12', '13', '14', '15', '16', '17'];
+    expect(Object.keys(results)).toEqual(expectedKeys);
+
+    for (let key in results) {
+      const count = results[key] / SAMPLE_SIZE;
+      expect(count).toBeCloseTo(1 / expectedKeys.length, 2);
     }
   });
 });
